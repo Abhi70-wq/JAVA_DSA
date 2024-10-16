bd4721587b017bf24bbdd9dcaa0806268ff80abc
Abhishek Yadav <142342296+Abhi70-wq@users.noreply.github.com>
2024-10-16 09:20:26 +0530

    Update f4.java

--- FUNCTIONS/f4.java
+++ FUNCTIONS/f4.java
@@ -18,7 +18,7 @@ public class f4 {
     }
     static boolean isprime(int n){
         for(int i=2;i<=Math.sqrt(n);i++){
-            if(n%1==0){
+            if(n%i==0){
                 return false;
             }
         }
